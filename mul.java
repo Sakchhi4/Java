2364ebce7165f5bebcc22f4ebeb07b14fee9226c
Sakchhi4 <me.sakchhi@gmail.com>
2026-01-19 22:28:34 +0530

    3rd commit

---
+++ mul.java
@@ -0,0 +1,12 @@
+public class mul {
+    public static void main(String args[]) {
+        int a = 5;
+        int b = 4;
+
+        int mul = a * b;
+
+        System.out.println("Multiplication = " + mul);
+    }
+}
+
+
