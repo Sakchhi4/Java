8771bdf02a68db56551033994db1e73a31431eb2
Sakchhi Kumari <sakchhi.me@gmail.com>
2026-01-20 11:13:03 +0530

    Create besti.java

---
+++ day 2/besti.java
@@ -0,0 +1,8 @@
+public class besti {
+  public static void main( String args[]){
+    System.out.println("Sanuta");
+    System.out.println("Tenzi");
+    System.out.println("Sanika");
+  }
+    
+}
