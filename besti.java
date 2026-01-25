ebb78101b27cb581e9949e19a547dda17d6a8d6d
Sakchhi Kumari <sakchhi.me@gmail.com>
2026-01-25 21:01:05 +0530

    Delete besti.java

--- besti.java
+++
@@ -1,8 +0,0 @@
-public class besti {
-  public static void main( String args[]){
-    System.out.println("Sanuta");
-    System.out.println("Tenzi");
-    System.out.println("Sanika");
-  }
-    
-}
