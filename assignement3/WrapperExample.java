b5c89c6fe06d66e83dc355f4e39ecc5f352e56c9
Sakchhi Kumari <sakchhi.me@gmail.com>
2026-01-27 23:24:32 +0530

    Create WrapperExample.java

---
+++ assignement3/WrapperExample.java
@@ -0,0 +1,13 @@
+class WrapperExample {
+
+    public static void main(String[] args) {
+
+ 
+        Float f = 25.78f;
+
+        int num = f.intValue();
+
+        System.out.println("Float value: " + f);
+        System.out.println("Integer value: " + num);
+    }
+}
