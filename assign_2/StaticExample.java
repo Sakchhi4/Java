fbb06571b0b4a291fe3443cc3a280b950a33485f
Sakchhi Kumari <sakchhi.me@gmail.com>
2026-01-27 23:20:09 +0530

    Create StaticExample.java

---
+++ assign_2/StaticExample.java
@@ -0,0 +1,14 @@
+class StaticExample {
+
+    static int number = 10;
+
+    static void show() {
+        System.out.println("Number is: " + number);
+    }
+
+    public static void main(String args[]) {
+
+        show();
+    }
+}
+
