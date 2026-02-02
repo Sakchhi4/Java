5cde4aa680bab6f020873272cb5f263874718f84
Sakchhi Kumari <sakchhi.me@gmail.com>
2026-02-02 09:32:31 +0530

    Create finalvariable.java

---
+++ day4/finalvariable.java
@@ -0,0 +1,12 @@
+public class finalvariable {
+    final int MAX=100;
+    void display(){
+        System.out.println("MAX value: " + MAX);
+
+    }
+    public static void main(String[] args) {
+        finalvariable obj =new finalvariable();
+        obj.display();
+    }
+    
+}
