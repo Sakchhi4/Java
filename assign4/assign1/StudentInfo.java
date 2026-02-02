66bafdb58eeec69757b77900b322ab390252c5bd
Sakchhi Kumari <sakchhi.me@gmail.com>
2026-02-02 23:16:45 +0530

    Delete assign4/assign1/StudentInfo.java

--- assign4/assign1/StudentInfo.java
+++
@@ -1,7 +0,0 @@
-class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Name: Sakchhi Chaudhary");
-        System.out.println("Roll No: 12345");
-        System.out.println("Address: Chandigarh, India");
-    }
-}
