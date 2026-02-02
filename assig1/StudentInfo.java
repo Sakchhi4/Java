8361f5427bc28cc7d50f99d5a0ca03b6535bb402
Sakchhi Kumari <sakchhi.me@gmail.com>
2026-02-02 23:18:03 +0530

    Add StudentInfo class with student details

---
+++ assig1/StudentInfo.java
@@ -0,0 +1,7 @@
+class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Name: Sakchhi Chaudhary");
+        System.out.println("Roll No: 12345");
+        System.out.println("Address: Chandigarh, India");
+    }
+}
