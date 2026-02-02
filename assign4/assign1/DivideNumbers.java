a126990f2459cd462e8466e501eaeb949c44eb12
Sakchhi Kumari <sakchhi.me@gmail.com>
2026-02-02 23:15:02 +0530

    Add DivideNumbers class with division by zero check

---
+++ assign4/assign1/DivideNumbers.java
@@ -0,0 +1,14 @@
+class DivideNumbers {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 0;
+
+        if (b == 0) {
+            System.out.println("Error: Division by zero is not allowed");
+        } else {
+            int result = a / b;
+            System.out.println("Result = " + result);
+        }
+    }
+}
