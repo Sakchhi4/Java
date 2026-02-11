6b0504177d99a8200644c3c9567ece78b4e6a4f7
Sakchhi Kumari <sakchhi.me@gmail.com>
2026-02-11 15:14:45 +0530

    Create MultipleCatchExample.java

---
+++ day8/MultipleCatchExample.java
@@ -0,0 +1,14 @@
+public class MultipleCatchExample {
+    public static void main (String[] args){
+        try {
+            String str = null ;
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e){
+            System.out.println("Error: Null pointer exception ! Object is null.");
+        } catch (Exception e){
+            System.out.println("Some other error occured.");
+        }
+        System.out.println("Program continues after exception handling .");
+    }
+}
