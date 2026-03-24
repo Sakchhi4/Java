b010ee8e9ad940060aeac21d2e148570e5c0e67c
Sakchhi Kumari <sakchhi.me@gmail.com>
2026-03-24 10:16:16 +0530

    Implement payment processing with CreditCard and UPI

---
+++ Scenario /num3.java
@@ -0,0 +1,49 @@
+abstract class Payment {
+    float amount;
+
+    Payment(float amount) {
+        this.amount = amount;
+    }
+
+    abstract float processPayment();
+}
+
+
+
+class CreditCardPayment extends Payment {
+
+    CreditCardPayment(float amount) {
+        super(amount);
+    }
+
+    float processPayment() {
+        return amount + (amount * 0.02f);
+    }
+}
+
+
+
+
+class UPIPayment extends Payment {
+
+    UPIPayment(float amount) {
+        super(amount);
+    }
+
+    float processPayment() {
+        return amount;
+    }
+}
+
+
+
+public class main_3 {
+    public static void main(String[] args) {
+
+        Payment p1 = new CreditCardPayment(1000f);
+        Payment p2 = new UPIPayment(1000f);
+
+        System.out.println(p1.processPayment());
+        System.out.println(p2.processPayment());
+    }
+}
